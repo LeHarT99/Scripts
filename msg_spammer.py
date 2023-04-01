f8c6d65f4edf32077eca043c0789ddda7b4ea8b8
LeHarT99 <xa3ro990515@gmail.com>
2023-03-31 23:52:28 -0400

    Posibilidad de parar el script Nuevo script de spam sin delay

--- msg_spammer.py
+++ msg_spammer.py
@@ -2,10 +2,9 @@ import pyautogui, keyboard
 from time import sleep
 
 a = input("Mensaje que deseas spammear: \n")
-print("Presione 'p' para parar")
+sleep(5)
 
 while True:
-    sleep(2)
     pyautogui.typewrite(a)
     pyautogui.press("enter")
     if keyboard.is_pressed("p"):
